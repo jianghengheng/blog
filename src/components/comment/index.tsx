1b877258ea3aec8e020e4a1faa65e3d8ee1afe5c
蒋衡 <wsjiangheng@163.com>
2023-05-24 22:45:37 +0800

    feat： 增加评论组件输入框以及按钮

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,18 +1,37 @@
 
-import { Button, Card, Col, Row } from 'antd'
+import { Button, Card, Col, Row, Input } from 'antd'
 import { useState, useEffect, memo } from 'react'
-
+const { TextArea } = Input;
 
 // 文章页面
 function Article() {
-
+    const [value, setValue] = useState<string>('')
+    const [showbutton, setShowBtuton] = useState<boolean>(false)
     useEffect(() => {
 
     }, [])
-
+    const onChange = (value: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setValue(value.target.value)
+    }
+    const focus = () => {
+        setShowBtuton(true)
+    }
+    const cancleButton = () => {
+        setShowBtuton(false)
+    }
     return (
-        <div>
-            <Card title='评论'></Card >
+        <div className='w-100%'>
+            <Card title='评论'>
+                <div>
+                    <TextArea onFocus={focus}  style={{ height: 100 }} showCount maxLength={100} value={value} onChange={onChange} placeholder='输入评论（Enter换行）' />
+                    {showbutton ? <div className='flex  justify-between mt-3%'><div>图片</div>  <Button type='primary'>发表评论</Button> </div> : <></>}
+                </div>
+                <div onClick={cancleButton}>
+                    2323
+                </div>
+
+
+            </Card >
         </div>
     )
 }
